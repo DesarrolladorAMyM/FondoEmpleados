2b69be083f67ef3374c3b2f6267c537f0581ac28
Jesus Gonzalez Guerrero - PRACTICANTE DESARROLLO <practdesarrollo@amym.local>
2026-05-28 12:35:22 -0500

    correccion en en las url y config

--- fondo/static/js/portal.js
+++ fondo/static/js/portal.js
@@ -440,7 +440,7 @@ async function verificarPaso1EnServidor() {
     formData.append('correo_registrado',document.getElementById('correo-reg').value.trim());
     formData.append('año',              document.getElementById('año').value.trim());
 
-    const response = await fetch('FondoEmpleados/PortalAsociados/verificar-paso1/', {   
+    const response = await fetch('/FondoEmpleados/PortalAsociados/verificar-paso1/', {   
       method: 'POST',
       headers: { 'X-CSRFToken': getCookie('csrftoken') },
       body: formData,
