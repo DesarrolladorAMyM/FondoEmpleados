0d09e340715774d635f4b1b019abc7cda313fc83
Jesus Gonzalez Guerrero - PRACTICANTE DESARROLLO <practdesarrollo@amym.local>
2026-05-28 10:42:59 -0500

    cambios en la verificacion paso 1 js

--- PortalAsociados/urls.py
+++ PortalAsociados/urls.py
@@ -7,6 +7,6 @@ from django.conf.urls.static import static
 # rutas 
 
 urlpatterns=[
-    path('PortalAsociados/', views.Asociados, name="PortalAsociados"),
-    path('verificar-paso1/', views.verificar_paso1, name='verificar_paso1'),
+    path('/FondoEmpleado/PortalAsociados/', views.Asociados, name="PortalAsociados"),
+    path('/FondoEmpleados/verificar-paso1/', views.verificar_paso1, name='verificar_paso1'),
 ]
