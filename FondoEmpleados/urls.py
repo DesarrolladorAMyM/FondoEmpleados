26292b2675f970fca89308cd13a6691ac2679198
Jesus Gonzalez Guerrero - PRACTICANTE DESARROLLO <practdesarrollo@amym.local>
2026-05-28 13:45:27 -0500

    cambio urls portal 3

--- FondoEmpleados/urls.py
+++ FondoEmpleados/urls.py
@@ -22,9 +22,9 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
    # path('', RedirectView.as_view(url='/FondoEmpleados/', permanent=False)), 
-    path('admin/', admin.site.urls),
+    path('FondoEmpleados/admin/', admin.site.urls),
     path('FondoEmpleados/', include('fondo.urls')),
-    path('PortalAsociados/', include('PortalAsociados.urls')),
-    path("anuncios/", include("anuncios.urls")),] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('FondoEmpleados/PortalAsociados/', include('PortalAsociados.urls')),
+    path("FondoEmpleados/anuncios/", include("anuncios.urls")),] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
 
